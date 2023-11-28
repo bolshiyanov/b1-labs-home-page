65a87cc69cac9868b131f2f2170180b7a66bdc41
Bolshiyanov <bolshiyanov@gmail.com>
2023-11-28 19:55:03 +0000

    3 commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
-  description: "This is my portfolio",
+  title: "B1-Labs growth hacking team",
+  description: "Unlock exponential business growth. Identify hidden bottlenecks. We,at B1-Labs, analyze global successes, guiding you to replicate strategies",
 };
 
 export default function RootLayout({
